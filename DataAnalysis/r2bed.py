52e887509d14843916eae2b37c693909cad9155c
couocu19 <1131088589@qq.com>
2022-11-03 15:00:54 +0800

    11.3

--- DataAnalysis/r2bed.py
+++ DataAnalysis/r2bed.py
@@ -7,8 +7,11 @@ import random
 
 def creat_chr_rates_histogram():
     x_data = [f"chr{i}" for i in range(1, 18)]
-    y_data1 = [0.15,0.21,0.2,0.22,0.2,0.2,0.2,0.2,0.2,0.2,0.22,0.21,0.21,0.21,0.21,0.2,0.02]
-    y_data = [0.61,0.7,0.7,0.66,0.67,0.65,0.66,0.66,0.65,0.68,0.67,0.65,0.69,0.7,0.67,0.68,0.65]
+    # y_data1 = [0.15,0.21,0.2,0.22,0.2,0.2,0.2,0.2,0.2,0.2,0.22,0.21,0.21,0.21,0.21,0.2,0.02]
+    # y_data = [0.61,0.7,0.7,0.66,0.67,0.65,0.66,0.66,0.65,0.68,0.67,0.65,0.69,0.7,0.67,0.68,0.65]
+
+    y_data1 = []
+    y_data = []
     # 正确显示中文和负号
     plt.rcParams["font.sans-serif"] = ["SimHei"]
     plt.rcParams["axes.unicode_minus"] = False
@@ -29,8 +32,20 @@ def creat_chr_rates_histogram():
 
 def creat_chr_rates_histogram1():
     x_data = [f"chr{i}" for i in range(1, 18)]
-    y_data1 = [0.61, 0.7, 0.7, 0.66, 0.67, 0.65, 0.66, 0.66, 0.65, 0.68, 0.67, 0.65, 0.69, 0.7, 0.67, 0.68, 0.65]
-    y_data2 = [0.66,0.774,0.77,0.74,0.74,0.714,0.733,0.73,0.72,0.75,0.75,0.72,0.76,0.77,0.74,0.75,0.66]
+    # y_data1 = [0.61, 0.7, 0.7, 0.66, 0.67, 0.65, 0.66, 0.66, 0.65, 0.68, 0.67, 0.65, 0.69, 0.7, 0.67, 0.68, 0.65]
+    # y_data2 = [0.66,0.774,0.77,0.74,0.74,0.714,0.733,0.73,0.72,0.75,0.75,0.72,0.76,0.77,0.74,0.75,0.66]
+
+    y_data1 = [0.8326152,0.9774910 ,0.9848241,0.9665906,0.9526101,0.9372892,0.9498469,0.9338977 ,0.9197978 ,0.9342194,0.9630063,0.9118337,0.9604124,0.9596001,0.9510406,0.9525940,0.6278343]
+    y_data2 = [0.1472952,0.1645052,0.1784284, 0.1551001,0.1560791,0.1577134,0.1534493,0.1548389,0.1514022, 0.1563297,0.1579701,0.1733778,0.1585548,0.1569945,0.1646371,0.1544175,0.1639446]
+    y_data3 = [0]*17
+    for i in range(0,17):
+        a = y_data1[i]+y_data2[i]
+        y_data3[i] = a
+    for a in y_data3:
+        print(a)
+
+    y_data4 = [0.98,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0.80]
+    #
     plt.rcParams["font.sans-serif"] = ["SimHei"]
     plt.rcParams["axes.unicode_minus"] = False
     bar_width = 0.35
@@ -38,10 +53,11 @@ def creat_chr_rates_histogram1():
     #for i in range(len(x_data)):
     plt.bar(index, y_data1, bar_width, # A班x轴数据起始位置为index序列 alpha表示透明度
             alpha=0.5, color='#c0737a',label='第一次比对') #肉粉色
-    plt.bar(index + bar_width, y_data2, bar_width,  # B班x轴起始位置与A班数据错开
+    plt.bar(index + bar_width, y_data4, bar_width,  # B班x轴起始位置与A班数据错开
                 alpha=0.5, color='#2c6fbb',label='两次比对后') #蓝色
 
     plt.xticks(index + bar_width / 2, x_data)  # index+bar_width/2 使得标签居中显示
+    plt.ylim(0.5,1)
     plt.title("第一次比对和第二次比对后的reads总覆盖度对比图")
     # 设置x轴标签名
     plt.xlabel("染色体编号")
@@ -101,8 +117,8 @@ def create_char_avg_rates():
 
 
 if __name__ == '__main__':
-    #creat_chr_rates_histogram1()
-    create_char_avg_rates()
+    creat_chr_rates_histogram1()
+    #create_char_avg_rates()
 
 
 
--- DataAnalysis/yeast-data-analysis/YeastStatist.py
+++ DataAnalysis/yeast-data-analysis/YeastStatist.py
@@ -1,9 +1,16 @@
 import pandas as pd
-
+import numpy as np
+import matplotlib.pyplot as plt
+import turtle
 
 def getYeastAreas():
     #df = pd.read_table("D:\python-study\DataAnalysis\data\mf4257-ftl.csv")
+    pd.set_option('display.max_columns', None)
+    # 显示所有行
+    pd.set_option('display.max_rows', None)
+
     df = pd.read_table("D:\\python-study\\DataAnalysis\\data\\new-covered.csv")
+    #print(df.loc[1])
 
 
    # print(df)
@@ -13,7 +20,7 @@ def getYeastAreas():
     #     print(key)
     #     print(value)
     classify = df.groupby(by = ['chrName'])
-    L1 = [[0 for _ in range (6)] for _ in range (17)]
+    L1 = [[0 for _ in range (7)] for _ in range (17)]
     L2 = [[0 for _ in range (10)] for _ in range (17)]
     i = 0
     for key,value in classify:
@@ -23,6 +30,7 @@ def getYeastAreas():
         min = value["length"].min()
         avg = value["length"].mean() # 均值
         median = value["length"].median() #中位数
+        qu = value["length"].quantile(q=0.75)
         sk = value["length"].skew() #偏态系数
         md = value["rank"].mode() #众数
         #print(max,min,median,sk,sep=' ')
@@ -31,17 +39,17 @@ def getYeastAreas():
         L1[i][1] = min
         L1[i][2] = avg
         L1[i][3] = median
-        L1[i][4] = sk
-        L1[i][5] = md
+        L1[i][4] = qu
+        L1[i][5] = sk
+        L1[i][6] = md
         #print(i)
         #print(L1[i][0], L1[i][1], L1[i][2], L1[i][3], sep=' ')
         i = i+1
 
-
     for a in L1:
-        # print(a[0],a[1],'%.3f'%a[2],a[3],'%.3f'%a[4],sep='  ')
-         print(a[5])
-    #     #print(a[0],a[1],a[2],a[3],sep=' ' )
+         print(a[0],a[1],'%.3f'%a[2],a[3],'%.3f'%a[5],sep='  ')
+         #print(a[5])
+        #print(a[0],a[1],a[2],a[3],sep=' ' )
 
     j = 0
     k = 0
@@ -55,14 +63,31 @@ def getYeastAreas():
             j = j+1
             k = 0
 
+    creatAreadistribution(L2)
+
 
     # for i in L2:
-    #     print(i)
+    #      print(i)
 
 
+def creatAreadistribution(data):
+    x_data = ['区域1','区域2','区域3','区域4','区域5','区域6','区域7','区域8','区域9','区域10']
+    plt.rcParams["font.sans-serif"] = ["SimHei"]
+    plt.rcParams["axes.unicode_minus"] = False
+    cololrs = ['red','#FF8C00','green','blue','pink','orange','brown','#008080','#DEB887','#8B008B','#000080','#32CD32','#708090','#4682B4','#7CFC00','#800080','#D2691E']
+    i = 0
+    for a in data:
+        y = a
+        b = i+1
+        plt.plot(x_data, y, color=cololrs[i],marker='o', linestyle='-', label='chr%d'%b)
+        #break
+        i = i+1
 
-
-
+    plt.legend()
+    plt.xlabel("每条染色体的分割区域")  # X轴标签
+    plt.ylabel("当前区域上的新区域区域的个数")  # Y轴标签
+    plt.ylim(0,700)
+    plt.show()
 
 
 if __name__ == '__main__':
